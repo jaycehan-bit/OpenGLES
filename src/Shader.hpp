f376dee2c834ef309cf7ffa963b7f76d01bf7a49
jaycehan <jaycehan@tencent.com>
2024-02-27 13:26:00 +0800

    开发部分着色器类

---
+++ src/Shader.hpp
@@ -0,0 +1,21 @@
+#ifndef SHADER_HPP
+#define SHADER_HPP
+#include <iostream>
+
+class Shader
+{
+public:
+    // 程序ID
+    unsigned int ID;
+    // 构造方法
+    // @param vertexPath 顶点着色器文件路径
+    // @param fragmentPath 片段着色器文件路径
+    Shader(const char *vertexPath, const char *fragmentPath);
+    ~Shader();
+    void userProgram();
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+};
+
+#endif

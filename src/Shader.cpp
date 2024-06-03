d817414d4af4802974a45a2761bc3f44dca0a482
jaycehan <jaycehan@tencent.com>
2024-06-03 23:27:09 +0800

    集成GLM

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -10,6 +10,7 @@
 #include <sstream>
 #include "glfw3.h"
 #include "Shader.hpp"
+#include "gtc/type_ptr.hpp"
 
 Shader::Shader(const char *vertexPath, const char *fragmentPath)
 {
@@ -64,6 +65,11 @@ void Shader::setInt(const std::string &name, int value) const
 void Shader::setFloat(const std::string &name, float value) const
 {
     glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setMat4(const std::string &name, glm::mat4 value) const
+{
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
 }
 
 void Shader::compileShaderProgram(const char *vertexCode, const char *fragmentCode)
--- src/Shader.hpp
+++ src/Shader.hpp
@@ -1,6 +1,7 @@
 #ifndef SHADER_HPP
 #define SHADER_HPP
 #include <iostream>
+#include "mat4x4.hpp"
 
 class Shader
 {
@@ -18,6 +19,7 @@ public:
     void setBool(const std::string &name, bool value) const;
     void setInt(const std::string &name, int value) const;
     void setFloat(const std::string &name, float value) const;
+    void setMat4(const std::string &name, glm::mat4 value) const;
 };
 
 #endif
